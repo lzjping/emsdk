30597d41f2ad2d245f57459bf3153d87be2caa3b
Paul <smartwp@126.com>
2016-03-01 17:05:36 +0800

    用户体系集成接口

--- em_test.go
+++ em_test.go
@@ -6,14 +6,19 @@ import (
 	"github.com/wpajqz/emsdk"
 )
 
+var (
+	client *emsdk.Client
+)
+
 func TestNew(t *testing.T) {
-	client, err := emsdk.New("links123", "linker", "YXA617WqYLg2EeWieSNUhFCrvg", "YXA6WCpzHwPQ8S2oMhozRy4PXFu2YGo")
+	var err error
+	client, err = emsdk.New("links123", "linker", "YXA617WqYLg2EeWieSNUhFCrvg", "YXA6WCpzHwPQ8S2oMhozRy4PXFu2YGo")
 	if err != nil {
 		if err == emsdk.ErrEM {
 			info := err.(emsdk.EMError)
 			t.Log(info.Code)
-			t.Log(info.Message)
 			t.Log(info.Description)
+			t.FailNow()
 		}
 		t.Fatal(err)
 	}
@@ -21,4 +26,49 @@ func TestNew(t *testing.T) {
 	if client == nil {
 		t.Fail()
 	}
+}
+
+func TestCreateUser(t *testing.T) {
+	result, err := client.CreateUser("217", "123456")
+	if err != nil {
+		if err == emsdk.ErrEM {
+			info := err.(emsdk.EMError)
+			t.Log(info.Code)
+			t.Log(info.Description)
+			t.FailNow()
+		}
+		t.Fatal(err)
+	}
+
+	t.Log("create user:", result)
+}
+
+func TestResetPassword(t *testing.T) {
+	result, err := client.ResetPassword("217", "456789")
+	if err != nil {
+		if err == emsdk.ErrEM {
+			info := err.(emsdk.EMError)
+			t.Log(info.Code)
+			t.Log(info.Description)
+			t.FailNow()
+		}
+		t.Fatal(err)
+	}
+
+	t.Log("reset password:", result)
+}
+
+func TestDeleteUser(t *testing.T) {
+	result, err := client.DeleteUser("217")
+	if err != nil {
+		if err == emsdk.ErrEM {
+			info := err.(emsdk.EMError)
+			t.Log(info.Code)
+			t.Log(info.Description)
+			t.FailNow()
+		}
+		t.Fatal(err)
+	}
+
+	t.Log("delete user:", result)
 }
--- user.go
+++ user.go
@@ -1,1 +1,67 @@
 package emsdk
+
+import (
+	"net/http"
+	"strings"
+	"time"
+)
+
+const retryInterval = 3 * time.Second
+
+func (c *Client) CreateUser(username, password string) (bool, error) {
+	data := `{"username":"` + username + `","password":"` + password + `"}`
+	_, err := c.sendRequest("users", strings.NewReader(data), "POST")
+	if err != nil {
+		if err == ErrEM {
+			info := err.(EMError)
+			if info.Code == http.StatusServiceUnavailable {
+				time.Sleep(retryInterval)
+				_, err := c.sendRequest("users", strings.NewReader(data), "POST")
+				if err != nil {
+					return false, err
+				}
+			}
+		}
+		return false, err
+	}
+	return true, err
+}
+
+func (c *Client) DeleteUser(username string) (bool, error) {
+	url := "users/" + username
+	_, err := c.sendRequest(url, strings.NewReader(""), "DELETE")
+	if err != nil {
+		if err == ErrEM {
+			info := err.(EMError)
+			if info.Code == http.StatusServiceUnavailable {
+				time.Sleep(retryInterval)
+				_, err := c.sendRequest(url, strings.NewReader(""), "DELETE")
+				if err != nil {
+					return false, err
+				}
+			}
+		}
+		return false, err
+	}
+	return true, err
+}
+
+func (c *Client) ResetPassword(username, password string) (bool, error) {
+	url := "users/" + username + "/password"
+	data := `{"newpassword":"` + password + `"}`
+	_, err := c.sendRequest(url, strings.NewReader(data), "PUT")
+	if err != nil {
+		if err == ErrEM {
+			info := err.(EMError)
+			if info.Code == http.StatusServiceUnavailable {
+				time.Sleep(retryInterval)
+				_, err := c.sendRequest(url, strings.NewReader(""), "DELETE")
+				if err != nil {
+					return false, err
+				}
+			}
+		}
+		return false, err
+	}
+	return true, err
+}
